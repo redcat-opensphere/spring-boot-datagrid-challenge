771f024d79a97e9ac27a819c118be0a7de6d92d8
user7 <david.gonzaloblanco@gruposantander.com>
2021-09-29 16:11:47 +0000

    Configuration update

--- src/main/java/com/redhat/developergames/config/InfinispanConfiguration.java
+++ src/main/java/com/redhat/developergames/config/InfinispanConfiguration.java
@@ -3,7 +3,6 @@ package com.redhat.developergames.config;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.infinispan.commons.marshall.ProtoStreamMarshaller;
 import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,15 +19,13 @@ public class InfinispanConfiguration {
          // Configure the weather cache to be created if it does not exist in the first call
          URI weatherCacheConfigUri = cacheConfigURI("weatherCache.xml");
 
+         // Configure the sessions cache to be created if it does not exist in the first call
+         URI sessionsCacheConfigUri = cacheConfigURI("sessionsCache.xml");
+
          b.remoteCache("weather")
                  .configurationURI(weatherCacheConfigUri);
-
-			// Ask the server to create this cache on startup
-			b.remoteCache("sessions").configuration(
-					"<distributed-cache name=\"sessions\"><encoding media-type=\"application/x-protostream\"/></distributed-cache>");
-
-			// Use protostream marshaller to serialize the sessions with Protobuf
-			b.remoteCache("sessions").marshaller(ProtoStreamMarshaller.class);
+         b.remoteCache("sessions")
+                 .configurationURI(sessionsCacheConfigUri);
 
       };
    }
